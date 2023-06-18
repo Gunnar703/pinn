8f4fd8effee405c716c8e0bf238f3182c1f758e5
Anthony LoRe Starleaf <aglorestarleaf@gmail.com>
2023-06-17 19:23:58 -0700

    finished training with all data

---
+++ plot_loss.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+loss_arr = np.loadtxt("loss.dat")
+
+steps = loss_arr[:, 0]
+physics_loss = loss_arr[:, 1]
+collocation_loss = loss_arr[:, 2:6].sum(axis=1)
+
+plt.figure()
+plt.plot(steps, physics_loss, label="Physics Loss", linewidth=1)
+plt.plot(steps, collocation_loss, label="Data Loss", linewidth=1)
+plt.title("Loss vs. Epoch")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.legend()
+plt.yscale("log")
+plt.show()
