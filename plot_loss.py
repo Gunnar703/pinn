d175bac09755ad731e9dd0cd9cb79331ad999c04
Anthony LoRe Starleaf <aglorestarleaf@gmail.com>
2023-06-22 22:45:26 -0700

    reorganize files

--- plot_loss.py
+++ plot_loss.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 loss_arr = np.loadtxt("loss.dat")
 
@@ -9,6 +10,9 @@ physics_loss = loss_arr[:, 1]
 velocity_loss = loss_arr[:, 2:4].sum(axis=1)
 position_loss = loss_arr[:, 4:6].sum(axis=1)
 # acceleration_loss = loss_arr[:, 6:].sum(axis=1)
+
+if not os.path.isdir("media"):
+    os.mkdir("media")
 
 plt.figure()
 plt.plot(steps, physics_loss, label="Physics Loss", linewidth=1)
@@ -20,5 +24,5 @@ plt.xlabel("Epoch")
 plt.ylabel("Loss")
 plt.legend()
 plt.yscale("log")
-plt.savefig("loss_plot.png")
+plt.savefig("media/loss_plot.png")
 plt.show()
