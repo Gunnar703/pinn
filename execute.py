cbc4ec32e1c57c7135b0182c03f8263de8ac39d1
Anthony LoRe Starleaf <aglorestarleaf@gmail.com>
2023-06-29 06:07:06 -0700

    increase training iters

--- execute.py
+++ execute.py
@@ -305,7 +305,7 @@ net = MsFNN(
 model = dde.Model(pde, net)
 model.compile(optimizer="adam", lr=5e-5, external_trainable_variables=E)
 losshistory, train_state = model.train(
-    iterations=50_000, callbacks=[variable, plotter_callback, resampler]
+    iterations=100_000, callbacks=[variable, plotter_callback, resampler]
 )
 
 model.compile(optimizer="L-BFGS", external_trainable_variables=E)
