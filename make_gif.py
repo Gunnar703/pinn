1068b171037b39c4add7c72f253e7f3f816ae1c3
Anthony LoRe Starleaf <aglorestarleaf@gmail.com>
2023-07-07 15:27:53 -0700

    go back

--- make_gif.py
+++ make_gif.py
@@ -1,20 +1,12 @@
 from PIL import Image
 import glob
-import os
 
 frames = []
 images = glob.glob("plots/training/epoch_*_prediction.png")
 
-
 def sort_by_number(filename):
-    number = int(
-        filename.split("_")[1].split("_")[0]
-    )  # Extract the number from the file name
+    number = int(filename.split('_')[1].split('_')[0])  # Extract the number from the file name
     return number
-
-
-if not os.path.isdir("media"):
-    os.mkdir("media")
 
 sorted_images = sorted(images, key=sort_by_number)
 
@@ -22,10 +14,10 @@ for image in sorted_images:
     frames.append(Image.open(image))
 
 frames[0].save(
-    "media/training.gif",
-    format="GIF",
+    'training.gif', 
+    format='GIF',
     append_images=frames[1:],
     save_all=True,
     duration=100,
-    loop=0,
+    loop=0
 )
--- plotter_callback.py
+++ plotter_callback.py
@@ -1,68 +1,22 @@
 import deepxde as dde
 from matplotlib import pyplot as plt
 import numpy as np
-from scipy import integrate
 import torch
 
 
 class PlotterCallback(dde.callbacks.Callback):
-    def __init__(self, period, filepath, data, E, t_max, u_max, plot_residual=False):
+    def __init__(
+        self, period, filepath, data, tsol, usol, E_learned, plot_residual=False
+    ):
         super().__init__()
         self.period = period
         self.filepath = filepath
         self.epoch = -1
         self.data = data
-        self.E = E
+        self.tsol = tsol
+        self.usol = usol
+        self.E_learned = E_learned
         self.plot_residual = plot_residual
-        self.T_MAX = t_max
-        self.U_MAX = u_max
-
-        self.k_basis = torch.Tensor(self.data["k_basis"]).to("cuda")
-        self.M = torch.Tensor(self.data["M"]).to("cuda")
-
-        t = data["t"]
-        self.du_t_1 = data["Vel_3_2D"]
-        self.du_t_3 = data["Vel_4_2D"]
-
-        self.u_1 = integrate.cumulative_trapezoid(self.du_t_1, t, initial=0)
-        self.u_3 = integrate.cumulative_trapezoid(self.du_t_3, t, initial=0)
-
-        self.du_tt_1, self.du_tt_3 = [np.zeros_like(self.du_t_1)] * 2
-        self.du_tt_1[1:] = (self.du_t_1[1:] - self.du_t_1[:-1]) / (t[1:] - t[:-1])
-        self.du_tt_3[1:] = (self.du_t_3[1:] - self.du_t_3[:-1]) / (t[1:] - t[:-1])
-        self.t = t
-
-        (
-            self.du_t_1_np,
-            self.du_t_3_np,
-            self.u_1_np,
-            self.u_3_np,
-            self.du_tt_1_np,
-            self.du_tt_3_np,
-        ) = (
-            self.du_t_1.copy(),
-            self.du_t_3.copy(),
-            self.u_1.copy(),
-            self.u_3.copy(),
-            self.du_tt_1.copy(),
-            self.du_tt_3.copy(),
-        )
-
-        (
-            self.du_t_1,
-            self.du_t_3,
-            self.u_1,
-            self.u_3,
-            self.du_tt_1,
-            self.du_tt_3,
-        ) = (
-            torch.Tensor(self.du_t_1).to("cuda"),
-            torch.Tensor(self.du_t_3).to("cuda"),
-            torch.Tensor(self.u_1).to("cuda"),
-            torch.Tensor(self.u_3).to("cuda"),
-            torch.Tensor(self.du_tt_1).to("cuda"),
-            torch.Tensor(self.du_tt_3).to("cuda"),
-        )
 
     def on_epoch_end(self):
         self.epoch += 1  # increment epoch counter
@@ -72,41 +26,75 @@ class PlotterCallback(dde.callbacks.Callback):
         fig.suptitle(
             f"Epoch: {self.epoch}"
             + "\n"
-            + "E = %.3f" % np.abs(float(self.E.detach().cpu().item()))
+            + "E = %.3f" % float(self.E_learned.detach().cpu().item())
             + "\n"
         )
 
+        residual = self.model.predict(
+            self.data["t"].reshape(-1, 1), operator=self.get_residual
+        )
         v_pred = self.model.predict(
             self.data["t"].reshape(-1, 1), operator=self.differentiate_model_output
         )
         for dim in range(4):
             ax = axes[dim]
-            ax.plot(self.t, v_pred[:, dim], "--", label="Prediction", color="purple")
 
+            ax.plot(self.data["t"], v_pred[:, dim], label="Prediction", color="black")
+            ax.plot(
+                self.data["t"],
+                residual[:, dim] * 1e-6,
+                label=r"Residual $\times 10^{-6}$",
+                color="purple",
+                linestyle="--",
+            )
+
+            # Plot Solution Data
+            ax.plot(
+                self.tsol,
+                self.usol[dim],
+                label="Solution (RK-45)",
+                color="gray",
+            )
+
+            # Plot given data
             if dim == 0:
-                ax.plot(self.t, self.data["Vel_3_1_2D"], label="Solution", color="gray")
-            elif dim == 1:
-                ax.plot(self.t, self.data["Vel_3_2D"], label="Solution", color="gray")
                 ax.plot(
-                    self.t,
-                    self.data["Vel_3_2D"],
+                    self.data["t"],
+                    self.data["Vel_3_1_2D"],
+                    label="Data (OPS)",
+                    marker="x",
+                    markersize=1,
                     linestyle="None",
-                    marker="+",
-                    markersize=2,
-                    label="Data",
+                    color="orange",
+                )
+            elif dim == 1:
+                ax.plot(
+                    self.data["t"],
+                    self.data["Vel_3_2D"],
+                    label="Data (OPS)",
+                    marker="x",
+                    markersize=1,
+                    linestyle="None",
                     color="orange",
                 )
             elif dim == 2:
-                ax.plot(self.t, self.data["Vel_4_1_2D"], label="Solution", color="gray")
-            elif dim == 3:
-                ax.plot(self.t, self.data["Vel_4_2D"], label="Solution", color="gray")
                 ax.plot(
-                    self.t,
-                    self.data["Vel_4_2D"],
+                    self.data["t"],
+                    self.data["Vel_4_1_2D"],
+                    label="Data (OPS)",
+                    marker="x",
+                    markersize=1,
                     linestyle="None",
-                    marker="+",
-                    markersize=2,
-                    label="Data",
+                    color="orange",
+                )
+            elif dim == 3:
+                ax.plot(
+                    self.data["t"],
+                    self.data["Vel_4_2D"],
+                    label="Data (OPS)",
+                    marker="x",
+                    markersize=1,
+                    linestyle="None",
                     color="orange",
                 )
 
@@ -120,8 +108,45 @@ class PlotterCallback(dde.callbacks.Callback):
         )
         plt.close()
 
+        # This shouldn't stay here, but for now it's convenient
+        if self.epoch == 200_000:
+            self.model.losshistory.set_loss_weights([1, 1, 1, 1, 1])
+
     def differentiate_model_output(self, x, y):
         ret = torch.zeros_like(y)
-        for i in range(y.shape[1]):
+        for i in range(4):
             ret[:, i] = dde.grad.jacobian(y, x, i=i, j=0).squeeze()
         return ret
+
+    def get_residual(self, t, u):
+        y = u
+        y_t = torch.zeros_like(y)
+        y_tt = torch.zeros_like(y)
+
+        for dim in range(4):
+            y_t[:, dim] = dde.grad.jacobian(u, t, i=dim, j=0).squeeze()
+            y_tt[:, dim] = dde.grad.hessian(u, t, component=dim).squeeze()
+
+        E = torch.abs(self.E_learned) * 1e7
+        K = torch.Tensor(self.data["k_basis"]) * E
+        C = (
+            self.data["Damp_param"][0] * torch.Tensor(self.data["M"])
+            + self.data["Damp_param"][1] * K
+        )
+
+        F = np.zeros((t.shape[0], u.shape[1]))
+        f_quasiscalar = self.force_magnitude(t.detach().cpu()).squeeze()
+        F[:, 3] = -f_quasiscalar
+        F = torch.Tensor(F)
+
+        residual = (
+            torch.mm(torch.Tensor(self.data["M"]), y_tt.permute((1, 0)))
+            + torch.mm(torch.abs(C), y_t.permute((1, 0)))
+            + torch.mm(torch.abs(K), y.permute((1, 0)))
+            - F.permute((1, 0))
+        ).permute((1, 0))
+
+        return residual
+
+    def force_magnitude(self, t):
+        return np.interp(t, self.data["t"], self.data["load"]) * 1e3
