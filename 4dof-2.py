dcb550aea375cc3151ec529daf9be7b983040efc
Anthony LoRe Starleaf <aglorestarleaf@gmail.com>
2023-07-07 17:34:55 -0700

    .t()

--- 4dof-2.py
+++ 4dof-2.py
@@ -17,9 +17,9 @@ def ode_system(x, y):
     """
     y_t, y_tt = derivatives(x, y)
     return (
-        torch.mm(data.TORCH_M, y_tt.transpose())
-        + torch.mm(data.TORCH_C, y_t.transpose())
-        + torch.mm(data.TORCH_K, y.transpose())
+        torch.mm(data.TORCH_M, y_tt.t())
+        + torch.mm(data.TORCH_C, y_t.t())
+        + torch.mm(data.TORCH_K, y.t())
         - data.F(x)
     )
 
