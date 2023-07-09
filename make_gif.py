19ec2d7b531f5bae5d4602988fb9ab859bb2ab0c
Anthony LoRe Starleaf <aglorestarleaf@gmail.com>
2023-07-09 06:30:10 -0700

    trained - not very good

--- make_gif.py
+++ make_gif.py
@@ -1,12 +1,17 @@
 from PIL import Image
 import glob
+import os
 
 frames = []
-images = glob.glob("plots/training/epoch_*_prediction.png")
+images = glob.glob("scp_destination/plots/training/epoch_*_prediction.png")
+
 
 def sort_by_number(filename):
-    number = int(filename.split('_')[1].split('_')[0])  # Extract the number from the file name
+    number = int(
+        filename.split(os.path.sep)[-1].split("_")[1].split("_")[0]
+    )  # Extract the number from the file name
     return number
+
 
 sorted_images = sorted(images, key=sort_by_number)
 
@@ -14,10 +19,10 @@ for image in sorted_images:
     frames.append(Image.open(image))
 
 frames[0].save(
-    'training.gif', 
-    format='GIF',
+    "training.gif",
+    format="GIF",
     append_images=frames[1:],
     save_all=True,
     duration=100,
-    loop=0
+    loop=0,
 )
